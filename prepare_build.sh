9f8070cb7985b74b7c711148f164c11e31fede8f
ben <eagleowlhawk@gmail.com>
2024-05-07 11:25:30 +0300

    AMGUI-3 Update am hash, fix size of window

--- prepare_build.sh
+++ prepare_build.sh
@@ -2,4 +2,4 @@ rm -rf aquamarine &&
 git clone https://github.com/MaksymT17/aquamarine.git &&
 cd aquamarine &&
 # hash of working commit which was used in test
-git checkout e96c385911e5172b1afdfe51fc06c44ce29cfac5
+git checkout 5a0a81dcbe1769cbf02ea2721a350eee09686ca4
