7dd8f8386207d64cd4fbb6dba5daad0772f5a46f
ben <eagleowlhawk@gmail.com>
2024-05-08 10:29:04 +0300

    AMGUI-4 Switch to emerald branch for cloning repo sources

--- prepare_build.sh
+++ prepare_build.sh
@@ -1,5 +1,2 @@
 rm -rf aquamarine &&
-git clone https://github.com/MaksymT17/aquamarine.git &&
-cd aquamarine &&
-# hash of working commit which was used in test
-git checkout 5a0a81dcbe1769cbf02ea2721a350eee09686ca4
+git clone --depth 1 -b emerald_0.2.1 https://github.com/MaksymT17/aquamarine.git
--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
-cp aquamarine/inputs/wiki_threaded_bfs.jpg build/ &&
-cp aquamarine/inputs/wiki_threaded_bfs.jpg build/ &&
-cp aquamarine/inputs/configuration.csv build/ &&
-cp aquamarine/inputs/rs_1.bmp build/ &&
-cp aquamarine/inputs/rs_2.bmp build/ &&
-cp aquamarine/inputs/20x20_clean.bmp build/ &&
-cp aquamarine/inputs/20x20_5objs.bmp build/ &&
-cp aquamarine/inputs/10x10_clean.bmp build/ &&
-cp aquamarine/inputs/10x10_2obj.bmp build/ &&
-echo "Test images are copied." &&
+cp wiki_threaded_bfs.jpg build/ &&
+cp configuration.csv build/ &&
+echo "Data has been copied." &&
 cd build &&
 ./am_gui
